421dd3b48e13b306483e1e6291c68a781d9dba43
DamienXP <66555302+DamienXP@users.noreply.github.com>
2020-07-20 14:16:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const bot = new Discord.Client()
 
 bot.on('ready', function () {
   console.log("Je suis connecté !")
-})
+});
 
-bot.login('NzM0NDY3MDI5Mjg4ODc4MTQy.XxTzDg.q18uqh03VTTpYhHUtf4TEUuioyc')
+bot.login("NzM0NDY3MDI5Mjg4ODc4MTQy.XxWCAA.dN-Lrkr7eUh0oR28XKmqytum0tk")
+
+bot.on('message', message => {
+    if (message.content === 'ping') { 
+      message.channel.send('pong !') 
+    }
+  });
+
+  bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+      return channel.send('Bienvenue sur mon serveur ' + member.displayName)
+    }).catch(console.error)
+ });
