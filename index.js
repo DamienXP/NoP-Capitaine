4c9d5474b829a8d5a29296346bf27616678bbbbb
DamienXP <66555302+DamienXP@users.noreply.github.com>
2020-07-23 15:44:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+﻿const Discord = require('discord.js')
+const { start } = require('repl')
+const bot = new Discord.Client()
+
+let prefix = "!"
+
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
+
+bot.login("NzM0NDY3MDI5Mjg4ODc4MTQy.XxjvdA.oputTA3TBJsMmgyN6S6KYXzKWt0")
+
+bot.on('message', message => {
+    if (message.content === '-instagram') {
+      message.channel.send('https://www.instagram.com/nexottv')
+    }
+  });
+
+  bot.on('message', message => {
+    if (message.content === '-invite') {
+      message.channel.send('https://discord.com/api/oauth2/authorize?client_id=734467029288878142&permissions=8&scope=bot')
+    }
+  });
+
+  bot.on('message', message => {
+    if (message.content === '-youtube') {
+      message.channel.send('https://www.youtube.com/c/nexottv')
+    }
+  });
+
+  bot.on('message', message => {
+    if (message.content === '-twitch') {
+      message.channel.send('https://www.twitch.tv/nexottv54')
+    }
+  });
+
+  bot.on('message', message => {
+    if (message.content === '-twitter') {
+      message.channel.send('https://twitter.com/NexoTTv1')
+    }
+  });
