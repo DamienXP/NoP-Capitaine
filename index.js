9192758a7eeb53a89fb810319be8a34d509145b0
DamienXP <66555302+DamienXP@users.noreply.github.com>
2020-07-23 03:53:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const Discord = require('discord.js')
+const { start } = require('repl')
 const bot = new Discord.Client()
+
+let prefix = "!"
 
 bot.on('ready', function () {
   console.log("Je suis connecté !")
-});
+})
 
-bot.login("NzM0NDY3MDI5Mjg4ODc4MTQy.XxWCAA.dN-Lrkr7eUh0oR28XKmqytum0tk")
+bot.login("NzM0NDY3MDI5Mjg4ODc4MTQy.XxWL9A.SBQLdk9svD4DIPhB5r09CmW96Oc")
 
 bot.on('message', message => {
-    if (message.content === 'ping') { 
-      message.channel.send('pong !') 
+    if (message.content === '-instagram') {
+      message.channel.send('https://www.instagram.com/nexottv')
     }
   });
 
-  bot.on('guildMemberAdd', member => {
-    member.createDM().then(channel => {
-      return channel.send('Bienvenue sur mon serveur ' + member.displayName)
-    }).catch(console.error)
- });
+  bot.on('message', message => {
+    if (message.content === '-invite') {
+      message.channel.send('https://discord.com/api/oauth2/authorize?client_id=734467029288878142&permissions=8&scope=bot')
+    }
+  });
+
+  bot.on('message', message => {
+    if (message.content === '-youtube') {
+      message.channel.send('https://www.youtube.com/c/nexottv')
+    }
+  });
+
+  bot.on('message', message => {
+    if (message.content === '-twitch') {
+      message.channel.send('https://www.twitch.tv/nexottv54')
+    }
+  });
+
+  bot.on('message', message => {
+    if (message.content === '-twitter') {
+      message.channel.send('https://twitter.com/NexoTTv1')
+    }
+  });
